15dfd16da997822e6feccb1808b2280b70a1a182
Irvin Lim <irvinlimweiquan@gmail.com>
2019-11-19 17:08:33 +0800

    Give generous upperbound for max_length of pub_key char field, because base64 does not always give the same length

--- sample-app/app/models.py
+++ sample-app/app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     uid = models.AutoField(primary_key=True)
-    pub_key = models.CharField(unique=True, max_length=344)  #len(JSEncrypt.getPublicBaseKeyB64()) = 334
+    pub_key = models.CharField(unique=True, max_length=1024)
