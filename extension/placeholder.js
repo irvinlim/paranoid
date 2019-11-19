905c169839863a4bc9eadcb86b201d20b51d7e31
Irvin Lim <irvinlimweiquan@gmail.com>
2019-11-19 16:03:14 +0800

    Refactor placeholder.js

--- extension/placeholder.js
+++ extension/placeholder.js
@@ -1,39 +1,38 @@
 /*
-Placeholder Replacement Logic
-*/
+ * Contains placeholder replacement logic, which will be executed on the service application's page.
+ * Attaches a closed shadow root to encapsulate private user fields, which will be inaccessible to the JS runtime.
+ */
 
-//Attach closed shadow root to each paranoid tag
-(async() => {
-    let service = await ParanoidStorage.getService(origin);
-    console.log(service);
-    if (service) {
-        //Get Paranoid Tags
-        let paranoid_tags = document.getElementsByTagName("paranoid");
-        console.log(paranoid_tags);
-        for (let i = 0; i < paranoid_tags.length; i++){
-            let current_tag = paranoid_tags[i];
-            let uid = current_tag.getAttribute("uid");
-            let attribute = current_tag.getAttribute("attribute");
-            console.log(uid);
-            console.log(service.uid);
-            console.log(service.foreign_map);
-            if(uid == service.uid || uid in service.foreign_map){
-                let unmasked_data = "";
-                if (uid == service.uid){
-                    unmasked_data = service.map[attribute];
-                } else {
-                    unmasked_data = service.foreign_map[uid][attribute];
-                }
-                //Clear placeholder contents and append span element to house shadow root
-                let replacement_span = document.createElement("span");
-                current_tag.innerHTML = "";
-                current_tag.appendChild(replacement_span);
+(async () => {
+  const service = await ParanoidStorage.getService(origin);
+  if (!service) {
+    return;
+  }
 
-                //Attach shadow root containing private data 
-                let shadow = replacement_span.attachShadow({mode: 'closed'});
-                let placeholder_replacement = document.createTextNode(unmasked_data);
-                shadow.appendChild(placeholder_replacement);
-            }
-        }
+  const tags = document.getElementsByTagName('paranoid');
+  for (let tag of tags) {
+    const uid = parseInt(tag.getAttribute('uid'));
+    const attribute = tag.getAttribute('attribute');
+    // console.log(uid);
+    // console.log(service.uid);
+    // console.log(service.foreign_map);
+    if (uid === service.uid || uid in service.foreign_map) {
+      let unmasked_data = '';
+      if (uid === service.uid) {
+        unmasked_data = service.map[attribute];
+      } else {
+        unmasked_data = service.foreign_map[uid][attribute];
+      }
+
+      // Clear placeholder contents and append span element to house shadow root
+      const replacement_span = document.createElement('span');
+      tag.innerHTML = '';
+      tag.appendChild(replacement_span);
+
+      // Attach shadow root containing private data
+      const shadow = replacement_span.attachShadow({ mode: 'closed' });
+      const placeholder_replacement = document.createTextNode(unmasked_data);
+      shadow.appendChild(placeholder_replacement);
     }
+  }
 })();
